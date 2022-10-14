7de54da351120e8e996bda5bfddc44ee55543081
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-10-14 10:07:43 +0530

    Update SampleApplication.java

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(Stri args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
